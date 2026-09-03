be4eb8b70d446ce1d3dc460849dedd29d20ab3b6
Ashok <ashoksiva135@gmail.com>
2026-09-03 16:22:00 +0530

    feat: add root / health route

--- src/routes/buildRoutes.js
+++ src/routes/buildRoutes.js
@@ -8,6 +8,9 @@ const {
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.json({ message: 'Mini-Vercel Worker is running!', status: 'ok' });
+});
 router.post('/build', triggerBuild);
 router.get('/status/:id', getBuildStatus);
 router.get('/stream/:id', streamBuildProgress);
